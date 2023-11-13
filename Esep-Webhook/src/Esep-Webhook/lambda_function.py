b94631a64e5382ec224894cf9774f5a92874d3da
Reece-McDonald <100727702+Reece-McDonald@users.noreply.github.com>
2023-11-13 13:47:00 -0500

    Update lambda_function.py

--- Esep-Webhook/src/Esep-Webhook/lambda_function.py
+++ Esep-Webhook/src/Esep-Webhook/lambda_function.py
@@ -3,7 +3,6 @@ import json
 import requests
 
 def lambda_handler(event, context):
-    input_data = event
-    issue_url = input_data['issue']['html_url']
+    issue_url = event['issue']['html_url']
     payload = {'text': f'Issue Created: {issue_url}'}
     response = requests.post(os.environ.get('SLACK_URL'), json=payload)
