84a92bb8b16160ba8a7b5ead4eb0d114b94072b1
Reece-McDonald <100727702+Reece-McDonald@users.noreply.github.com>
2023-11-13 13:51:47 -0500

    Update lambda_function.py

--- Esep-Webhook/src/Esep-Webhook/lambda_function.py
+++ Esep-Webhook/src/Esep-Webhook/lambda_function.py
@@ -1,8 +1,12 @@
-import os
 import json
+import os
 import requests
 
 def lambda_handler(event, context):
+    # issue_url grabs the url of the issue that has been interacted with.
     issue_url = event['issue']['html_url']
+    # payload generates the json 'text' to be printed in slack, being 'Issue Created: issue_url'.
     payload = {'text': f'Issue Created: {issue_url}'}
+    # response uses the requests library to send the text to the slack channel, using the environment variable
+    # that contains the URL to post to the channel, and sets the json data to the payload data.
     response = requests.post(os.environ.get('SLACK_URL'), json=payload)
