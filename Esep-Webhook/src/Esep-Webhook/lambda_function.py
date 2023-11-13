9d169fc3a167520367ec689f0687927c327a84b2
Reece-McDonald <100727702+Reece-McDonald@users.noreply.github.com>
2023-11-12 22:04:48 -0500

    Update lambda_function.py

--- Esep-Webhook/src/Esep-Webhook/lambda_function.py
+++ Esep-Webhook/src/Esep-Webhook/lambda_function.py
@@ -1,43 +1,23 @@
 import json
 import os
-import http.client
+import urllib.request
+from http.client import HTTPResponse
 
 def lambda_handler(event, context):
-    # Log the received event for debugging
-    print(f"Received event: {event}")
-
-    # Check if event is not an empty string
-    if not event:
-        return "Error: Empty input event"
-
     try:
-        # Try to deserialize JSON
         json_data = json.loads(event)
     except json.JSONDecodeError as e:
-        # Log the error for debugging
-        print(f"Error decoding JSON: {str(e)}")
-        return f"Error decoding JSON: {str(e)}"
+        return {
+            "errorMessage": f"Error decoding JSON: {str(e)}",
+            "errorType": "JSONDecodeError"
+        }
 
-    # Create payload string
-    issue_url = json_data.get('issue', {}).get('html_url', '')
-    payload = f"{{'text':'Issue Created: {issue_url}'}}"
+    payload = f"{{'text':'Issue Created: {json_data['issue']['html_url']}'}}"
 
-    # Set up HTTP connection
-    slack_url = os.environ.get("SLACK_URL")  # Get Slack URL from environment variables
-    url_parts = slack_url.replace("https://", "").split("/")
-    conn = http.client.HTTPSConnection(url_parts[0])
+    url = os.environ.get("SLACK_URL")
+    request_data = payload.encode("utf-8")
+    headers = {"Content-Type": "application/json"}
 
-    # Set up headers
-    headers = {'Content-Type': 'application/json'}
-
-    # Send POST request
-    conn.request("POST", "/" + "/".join(url_parts[1:]), body=payload, headers=headers)
-
-    # Get response
-    response = conn.getresponse()
-    result = response.read().decode('utf-8')
-
-    # Close the connection
-    conn.close()
-
-    return result
+    request = urllib.request.Request(url, data=request_data, headers=headers, method="POST")
+    with urllib.request.urlopen(request) as response:  # type: HTTPResponse
+        return response.read().decode("utf-8")
