4b3ba9e068a581ae68d014beac4838cd08732f8b
Reece-McDonald <100727702+Reece-McDonald@users.noreply.github.com>
2023-11-12 23:15:16 -0500

    Update lambda_function.py

--- Esep-Webhook/src/Esep-Webhook/lambda_function.py
+++ Esep-Webhook/src/Esep-Webhook/lambda_function.py
@@ -4,36 +4,25 @@ import requests
 
 def lambda_handler(event, context):
     try:
-        # Parse the GitHub webhook event
-        data = json.loads(event['body'])
-        repository_name = data['repository']['full_name']
-        issue_title = data['issue']['title']
-        issue_url = data['issue']['html_url']
-
+        # Parse the JSON input
+        input_data = json.loads(event)
+        
+        # Extract relevant information from the JSON data
+        issue_url = input_data['issue']['html_url']
+        
         # Customize the Slack message format
-        slack_message = {
-            'text': f'New issue in {repository_name}: *{issue_title}* - {issue_url}'
-        }
+        payload = {'text': f'Issue Created: {issue_url}'}
 
         # Set your Slack incoming webhook URL
         slack_url = os.environ.get('SLACK_URL')
 
-        # Send the message to Slack
-        response = requests.post(slack_url, json=slack_message)
+        # Send the message to Slack using the requests library
+        response = requests.post(slack_url, json=payload)
 
         if response.status_code == 200:
-            return {
-                'statusCode': 200,
-                'body': json.dumps({'message': 'Message sent to Slack successfully'})
-            }
+            return {'statusCode': 200, 'body': json.dumps({'message': 'Message sent to Slack successfully'})}
         else:
-            return {
-                'statusCode': 500,
-                'body': json.dumps({'error': 'Failed to send message to Slack'})
-            }
+            return {'statusCode': 500, 'body': json.dumps({'error': 'Failed to send message to Slack'})}
 
     except Exception as e:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': str(e)})
-        }
+        return {'statusCode': 400, 'body': json.dumps({'error': str(e)})}
